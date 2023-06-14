ec60e81dfad37eea3e403703ebf5e2cf3ac70578
RamonBenahim <ramonbenahim@gmail.com>
2023-06-14 07:26:40 -0300

    initial commit

---
+++ src/main/java/com/ecommerce/huskycommerce/HuskycommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.huskycommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuskycommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuskycommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/huskycommerce/HuskycommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.huskycommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuskycommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
