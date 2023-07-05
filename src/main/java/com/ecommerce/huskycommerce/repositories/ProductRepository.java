e26522d39222335af1396ec850775336e42fcf1f
RamonBenahim <ramonbenahim@gmail.com>
2023-07-05 00:23:00 -0300

    get product by id

---
+++ src/main/java/com/ecommerce/huskycommerce/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.ecommerce.huskycommerce.repositories;
+
+import com.ecommerce.huskycommerce.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
