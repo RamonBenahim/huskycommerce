2bd751b3117a62b66b898617b41dd2998597fede
RamonBenahim <ramonbenahim@gmail.com>
2023-11-08 21:27:27 -0300

    initial commit

--- src/main/java/com/ecommerce/huskycommerce/services/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.huskycommerce.services.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-  public ResourceNotFoundException(String msg) {
-    super(msg);
-  }
-}
