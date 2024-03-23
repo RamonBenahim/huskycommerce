b961e21861df0029b4ee2eca5a1eb055348cba15
RamonBenahim <ramonbenahim@gmail.com>
2024-03-23 02:31:39 -0300

    feat: orders

--- src/main/java/com/ecommerce/huskycommerce/repositories/OrderRepository.java
+++ src/main/java/com/ecommerce/huskycommerce/repositories/OrderRepository.java
@@ -1,18 +1,8 @@
 package com.ecommerce.huskycommerce.repositories;
-
-import java.util.List;
-import java.util.Optional;
-
 import com.ecommerce.huskycommerce.entities.Order;
-import com.ecommerce.huskycommerce.projections.UserDetailsProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import com.ecommerce.huskycommerce.entities.User;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
-
 }
--- src/main/java/com/ecommerce/huskycommerce/services/OrderService.java
+++ src/main/java/com/ecommerce/huskycommerce/services/OrderService.java
@@ -1,9 +1,9 @@
 package com.ecommerce.huskycommerce.services;
 
 import com.ecommerce.huskycommerce.dto.OrderDTO;
-import com.ecommerce.huskycommerce.dto.ProductDTO;
-import com.ecommerce.huskycommerce.entities.Order;
-import com.ecommerce.huskycommerce.entities.Product;
+import com.ecommerce.huskycommerce.dto.OrderItemDTO;
+import com.ecommerce.huskycommerce.entities.*;
+import com.ecommerce.huskycommerce.repositories.OrderItemRepository;
 import com.ecommerce.huskycommerce.repositories.OrderRepository;
 import com.ecommerce.huskycommerce.repositories.ProductRepository;
 import com.ecommerce.huskycommerce.services.exceptions.ResourceNotFoundException;
@@ -11,14 +11,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
+
 @Service
 public class OrderService {
   @Autowired
   private OrderRepository repository;
+
+  @Autowired
+  private ProductRepository productRepository;
+
+  @Autowired
+  private OrderItemRepository orderItemRepository;
+
+  @Autowired
+  private UserService userService;
+
   @Transactional(readOnly = true)
   public OrderDTO findById(Long id) {
     Order order = repository.findById(id).orElseThrow(
             () -> new ResourceNotFoundException("Resource not found."));
     return new OrderDTO(order);
   }
+
+  @Transactional
+  public OrderDTO insert(OrderDTO dto) {
+    Order order = new Order();
+
+    order.setMoment(Instant.now());
+    order.setStatus(OrderStatus.WAITING_PAYMENT);
+    User user = userService.authenticated();
+    order.setClient(user);
+
+    for (OrderItemDTO itemDto : dto.getItems()) {
+      Product product = productRepository.getReferenceById(itemDto.getProductId());
+      OrderItem item = new OrderItem(order, product, itemDto.getQuantity(), product.getPrice());
+      order.getItems().add(item);
+    }
+
+    repository.save(order);
+    orderItemRepository.saveAll(order.getItems());
+
+    return new OrderDTO(order);
+  };
 }
